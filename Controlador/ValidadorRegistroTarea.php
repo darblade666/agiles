7255c4a7f132631e560b03a527e20146ac366763
Pavillondv7 <Pavillondv7@Marce>
2015-04-20 22:42:56 -0400

    Agregado las validaciones que faltaban

--- Controlador/ValidadorRegistroTarea.php
+++ Controlador/ValidadorRegistroTarea.php
@@ -1,1 +1,8 @@
-holahkhk
+<?php
+    $FechaEntrega = date("Y-m-d",strtotime($_POST['date']));
+    $NombreTarea = $_POST['name'];
+    $NombreMateria = $_POST['materia'];
+    $archivo = $_FILES["archivo_adjunto"];
+    $Descripcion = $_POST['descripcion'];
+    
+?>
